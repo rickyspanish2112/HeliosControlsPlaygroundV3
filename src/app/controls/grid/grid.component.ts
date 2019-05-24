ed8d8100d450593e8cf2125683118a70b2d844e8
Ross Starkey <ross.starkey@asm.org.uk>
2019-05-24 10:19:58 +0100

    tidy up

--- src/app/controls/grid/grid.component.ts
+++ src/app/controls/grid/grid.component.ts
@@ -71,9 +71,7 @@ export class GridComponent implements OnInit {
   removeAt(index: number) {
     const data = this.dataSource.data;
     data.splice(this.paginator.pageIndex * this.paginator.pageSize + index, 1);
-
     this.dataSource.data = data;
-
     this.expanded = false;
   }
 
