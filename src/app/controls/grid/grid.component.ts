d7664a75ed20ced2b716ced93e465e3b97a56c7d
Ross Starkey <ross.starkey@asm.org.uk>
2019-05-29 14:36:11 +0100

    removed pagination

--- src/app/controls/grid/grid.component.ts
+++ src/app/controls/grid/grid.component.ts
@@ -80,7 +80,7 @@ export class GridComponent implements OnInit {
 
   removeAt(index: number) {
     const data = this.dataSource.data;
-    data.splice(this.paginator.pageIndex * this.paginator.pageSize + index, 1);
+    data.splice(index, 1);
     this.dataSource.data = data;
     this.expanded = false;
   }
